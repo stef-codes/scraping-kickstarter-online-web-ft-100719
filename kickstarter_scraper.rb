1e4d7ce6ee3cd11e3fc865a3375dafe3a4887069
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-27 07:33:16 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,9 +13,10 @@ def create_project_hash
     projects[project] 
     
   #add title
-  
-  
+  title = project.css("h2.bbcard_name strong a").text
+  projects[title.to_sym] = {}
   end
+
  
   # return the projects hash
   projects
