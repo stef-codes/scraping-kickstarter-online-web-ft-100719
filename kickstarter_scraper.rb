17ccd3380e26666b3c7c52d3bf379dcae83d4f7d
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-27 07:29:32 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,7 @@ def create_project_hash
   # title: project.css("h2.bbcard_name strong a").text
   # image link:project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 end
 
 create_project_hash
