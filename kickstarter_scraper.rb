9eb179232e0bbf6d90d8791fcda0b184d1e33485
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-27 07:39:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -23,7 +23,11 @@ def create_project_hash
   :description => project.css("p.bbcard_blurb").text,
   
   #add location
-  project.css("ul.project-meta span.location-name").text,
+  :location => project.css("ul.project-meta span.location-name").text,
+  
+  #add percent_funded
+  
+  
   # return the projects hash
   projects
   # projects: kickstarter.css("li.project.grid_4")
