1c6851ab3b481431bf8fb5e6e4ab1df265e9b8d2
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-27 07:23:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,8 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
+  
+  # projects: kickstarter.css("li.project.grid_4")
 end
 
 create_project_hash
