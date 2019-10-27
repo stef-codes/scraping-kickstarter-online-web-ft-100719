1e520551b53bbf70dc97b8a661ae07756da56318
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-27 07:26:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,8 @@ def create_project_hash
   
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
+  project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
 end
 
 create_project_hash
